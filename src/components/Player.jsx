61fc1a7d28ea302c80301195dd012c1ce9290ca8
GurjeetSingh7000 <101127973+GurjeetSingh7000@users.noreply.github.com>
2024-04-27 14:21:14 +0530

    added name and diplay that

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export function Player(){
+    const [playerName , setPlayerName] = useState("");
+    const [submitted , setSubmitted] = useState(false);
+    const handleChange = (event) =>{
+        setSubmitted(false)
+        setPlayerName(event.target.value);
+    }
+
+    const handleClick = () =>{
+        setSubmitted(true);
+    }
     return (
         <section id="player">
-            <h2>Welcome unknown entity</h2>
+            <h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
+            
             <p>
-                <input type="text"></input>
-                <button>Set Name</button>
+                <input type="text" onChange={handleChange} value={playerName}></input>
+                <button onClick={handleClick}>Set Name</button>
             </p>
         </section>
     );
